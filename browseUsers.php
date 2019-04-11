00ed5b7957c552c2b6351beec69ff434248c6426
Jacob Forte <jforte7@kent.edu>
2019-04-11 17:41:24 -0400

    Initialize project structure

---
+++ browseUsers.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <?php include("php/head.inc.php"); ?>
+        <title>Some Title</title>
+    </head>
+    <body>
+        <?php include("php/header.inc.php"); ?>
+
+        <main>
+
+        </main>
+        
+        <?php include("php/footer.inc.php"); ?>
+    </body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv='content-type' content='text/html;charset=utf-8' />
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+<link rel="stylesheet" href="css/mainCSSFile.css">
