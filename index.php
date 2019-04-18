1cb2524919acb5cca7dd06ce059dc67df56c2d2e
Zach <contact@zachbrockway.com>
2019-04-18 16:39:53 -0400

    Fix file locations

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <?php include("php/head.inc.php"); ?>
+        <?php include("php/head.php"); ?>
         <title>Some Title</title>
     </head>
     <body>
-        <?php include("php/header.inc.php"); ?>
+        <?php include("php/header.php"); ?>
 
         <main>
 
         </main>
         
-        <?php include("php/footer.inc.php"); ?>
+        <?php include("php/footer.php"); ?>
     </body>
 </html>
