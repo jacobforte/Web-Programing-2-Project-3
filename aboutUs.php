7aca1a7f448353b9b05ad15a513c156b4ef85d36
mjohn113 <mjohn113@kent.edu>
2019-04-19 17:06:38 -0400

    done

--- aboutUs.php
+++ aboutUs.php
@@ -1,16 +1,59 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <?php include("php/head.inc.php"); ?>
-        <title>Some Title</title>
+        <?php include("php/head.php"); ?>
+        <title>About Us</title>
     </head>
     <body>
-        <?php include("php/header.inc.php"); ?>
+        <?php include("php/header.php"); ?>
 
-        <main>
+        <main class="container">
+		
+			<div class="row">
+			
+				<div class="container" style="padding-right: 0px;" >
+				
+					<h1>About us</h1>
+					
+					<div>
+					
+						<p>This is a website created for the final project for Web programming 2</p>
+						<p>It was Created by Jacob, Zach, Marcus</p>
+						
+						<br></br>
+						
+						<h2>Who Implemented What</h2>
+						<br>
+						
+						<h4>Jacob</h4>
+						<p>Search Results</p>
+						<p>Advanced Search</p>
+						<p>Display Single Travel Image</p>
+						<p>Browse Travel Images</p>
+						
+						<h4>Zach</h4>
+						<p>Home Page</p>
+						<p>Navigation (header)</p>
+						<p>Simple Search</p>
+						<p>Browse Post / User </p>
+						
+						<h4>Marcus</h4>
+						<p>About Us page</p>
+						<p>Display Single Post</p>
+						<p>Add To Favorites </p>
+						<p>View Favorites</p>
+						<p>Display Single Country / City </p>
+						
+						
+						
+						
+					</div>
+				</div>
+				
+			</div>
 
         </main>
         
-        <?php include("php/footer.inc.php"); ?>
+        <?php include("php/footer.php"); ?>
     </body>
 </html>
