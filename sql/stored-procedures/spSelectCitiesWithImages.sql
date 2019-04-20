1b5420a4bb606524244a361bb6167fdd7e9738c0
Jacob Forte <jforte7@kent.edu>
2019-04-20 16:43:46 -0400

    Finished advanced search

--- sql/stored-procedures/spSelectCitiesWithImages.sql
+++ sql/stored-procedures/spSelectCitiesWithImages.sql
@@ -8,6 +8,7 @@ BEGIN
 
     SELECT AsciiName as name, GeoNameID as ID FROM geocities
     RIGHT JOIN travelimagedetails ON geocities.GeoNameID = travelimagedetails.CityCode
+    WHERE AsciiName > ""
     GROUP BY GeoNameID;
 
 END$$
