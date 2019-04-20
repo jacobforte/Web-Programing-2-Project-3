8693effd6bdd9b4c2d2abe8290196ee80ede52e5
zbrockway <zachobrockway@gmail.com>
2019-04-20 12:14:46 -0400

    Add image display functionality on home page

---
+++ php/home/home.functions.php
@@ -0,0 +1,31 @@
+<?php
+
+define('_PATH', $_SERVER["DOCUMENT_ROOT"] . "/Web-Programing-2-Project-3");
+
+require( _PATH . "/php/" . "dbconnection.function.php");
+
+function outputTopImages() {
+    $data = dbconnection("spSelectTopImages()");
+
+    for ($i = 0; $i < 8; $i++) {
+        echo '<div class="col-6 col-sm-4 col-md-3 mb-4">
+            <a href="singleImage.php?id=' . $data[$i]['ImageID'] . '">
+                <img src="./travel-images/square-medium/' . $data[$i]['Path'] . '" class="img-thumbnail">
+            </a>
+        </div>';
+    }
+
+}
+
+function outputNewImages() {
+    $data = dbconnection("spSelectNewImages()");
+
+    for ($i = 0; $i < 8; $i++) {
+        echo '<div class="col-6 col-sm-4 col-md-3 mb-4">
+            <a href="singleImage.php?id=' . $data[$i]['ImageID'] . '">
+                <img src="./travel-images/square-medium/' . $data[$i]['Path'] . '" class="img-thumbnail">
+            </a>
+        </div>';
+    }
+
+}
--- sql/stored-procedures/runAllSPs.sql
+++ sql/stored-procedures/runAllSPs.sql
@@ -1,3 +1,5 @@
 source spSelectImages.sql;
 source spSelectPosts.sql;
 source spSelectUser.sql;
+source spSelectTopImages.sql;
+source spSelectNewImages.sql;
