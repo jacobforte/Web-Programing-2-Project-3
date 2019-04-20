1dc9635f5d1074f1bec8d8e9094b69ad62f130ee
Zach <contact@zachbrockway.com>
2019-04-20 12:57:59 -0400

    Add sidebar include

---
+++ php/home/sidebar.inc.php
@@ -0,0 +1,50 @@
+<div class="col-lg-3 col-12">
+    <div class="row mb-4">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header bg-pan">
+                    <p class="mb-0 text-white">Search</p>
+                </div>
+                <div class="card-body">
+                    <form action="" method="post">
+                        <div class="input-group">
+                            <input type="text" name="term" class="form-control">
+                            <div class="input-group-append">
+                                <button type="submit" name="searchSubmit" class="btn btn-primary"><i class="fas fa-search"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mb-4">
+        <div class="col-12">
+            <ul class="list-group">
+                <li class="list-group-item bg-pan text-white">Continents</li>
+                <li class="list-group-item"><a href="browse-images.php?continent=AF" class="link-pan">Africa</a></li>
+                <li class="list-group-item"><a href="browse-images.php?continent=AS" class="link-pan">Asia</a></li>
+                <li class="list-group-item"><a href="browse-images.php?continent=EU" class="link-pan">Europe</a></li>
+                <li class="list-group-item"><a href="browse-images.php?continent=NA" class="link-pan">North America</a></li>
+                <li class="list-group-item"><a href="browse-images.php?continent=OC" class="link-pan">Oceania</a></li>
+                <li class="list-group-item"><a href="browse-images.php?continent=SA" class="link-pan">South America</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="row mb-4">
+        <div class="col-12">
+            <ul class="list-group">
+                <li class="list-group-item bg-pan text-white">Popular</li>
+                <li class="list-group-item"><a href="country.php?id=CA" class="link-pan">Canada</a></li>
+                <li class="list-group-item"><a href="country.php?id=IT" class="link-pan">Italy</a></li>
+                <li class="list-group-item"><a href="country.php?id=DE" class="link-pan">Germany</a></li>
+                <li class="list-group-item"><a href="country.php?id=GH" class="link-pan">Ghana</a></li>
+                <li class="list-group-item"><a href="country.php?id=GR" class="link-pan">Greece</a></li>
+                <li class="list-group-item"><a href="country.php?id=HU" class="link-pan">Hungary</a></li>
+                <li class="list-group-item"><a href="country.php?id=ES" class="link-pan">Spain</a></li>
+                <li class="list-group-item"><a href="country.php?id=GB" class="link-pan">United Kingdom</a></li>
+                <li class="list-group-item"><a href="country.php?id=US" class="link-pan">United States</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
