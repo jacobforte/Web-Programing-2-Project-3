bebffeede17ad7a813f432fa3fdcea468c457e7e
Jacob Forte <jforte7@kent.edu>
2019-04-21 18:44:04 -0400

    Update spSelectAllImages.sql

--- sql/stored-procedures/spSelectAllImages.sql
+++ sql/stored-procedures/spSelectAllImages.sql
@@ -5,13 +5,13 @@ DROP PROCEDURE IF EXISTS spSelectAllImages$$
 CREATE PROCEDURE spSelectAllImages()
 
 BEGIN
-    SELECT travelImage.ImageID,
-        travelImage.Path,
+    SELECT travelimage.ImageID,
+        travelimage.Path,
         travelimagedetails.Title,
         geocountries.CountryName,
         geocontinents.ContinentName
-    FROM travelImage
-    LEFT OUTER JOIN travelimagedetails ON travelImage.ImageID = travelimagedetails.ImageID
+    FROM travelimage
+    LEFT OUTER JOIN travelimagedetails ON travelimage.ImageID = travelimagedetails.ImageID
     LEFT OUTER JOIN geocountries ON travelimagedetails.CountryCodeISO = geocountries.ISO
     LEFT OUTER JOIN geocontinents ON geocountries.Continent = geocontinents.ContinentCode;
 END$$
