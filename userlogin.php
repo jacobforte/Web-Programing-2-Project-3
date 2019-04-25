9966ba5d5575367de074a510c7e30f28eccfba78
mjohn113 <mjohn113@kent.edu>
2019-04-25 15:14:09 -0400

    login pages

---
+++ userlogin.php
@@ -0,0 +1,25 @@
+<?php
+
+			 include_once("php/dbconnection.function.php");
+			 session_start();
+			 
+
+				
+				$usernam = $_POST['email'];
+				$userpass= $_POST['password'];
+				
+				$row = dbconnection("spSelectUser('$usernam','$userpass')");
+				
+				if($row[0] != NULL){
+				
+					$_SESSION['username'] = $row[0]['UserName'];
+					$_SESSION['userpass'] = $row[0]['Password'];\
+					header('Location: index.php');
+				
+				}
+				else{
+					header('location: login.php');
+				}
+				
+			
+?>
