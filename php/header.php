1757c86d3ef283b1da3974cc1ca6c9a170e63788
mjohn113 <mjohn113@kent.edu>
2019-04-25 15:33:38 -0400

    will now display info on login
    now displays the user email instead of more and changes the login link to logout if user is logged in

--- php/header.php
+++ php/header.php
@@ -107,13 +107,26 @@
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="moreDropDown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    More
+				<?php
+					if(isset($_SESSION['username'])){
+						echo $_SESSION['username'];
+					}
+					else 
+						echo "More";
+				?>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="moreDropDown">
                     <a class="dropdown-item" href="favorites.php">View Favorites</a>
                     <a class="dropdown-item" href="#">My Account</a>
-                    <a class="dropdown-item" href="#">Register</a>
-                    <a class="dropdown-item" href="#">Login</a>
+                    <a class="dropdown-item" href="register.php">Register</a>
+				<?php
+					if(isset($_SESSION['username'])){
+						echo '<a class="dropdown-item" href="login.php?logout=1">Logout</a>';					
+					}
+					else{ 
+						echo '<a class="dropdown-item" href="login.php">Login</a>';						
+					}
+				?>
                 </div>
             </li>
         </ul>
